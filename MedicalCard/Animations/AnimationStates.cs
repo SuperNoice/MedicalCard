92faed320529bc1d68813cf8bfce6edc40fc7c8f
SuperNoice <SuperNoice@yandex.ru>
2021-11-15 20:27:00 +0300

    Изменены классы анимаций. Добавлен класс анимации строк grid контейнера.

---
+++ MedicalCard/Animations/AnimationStates.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalCard.Animations
+{
+    public enum AnimationState
+    {
+        Open, Close, Opening, Closing
+    }
+}
---
+++ MedicalCard/Animations/GridColumnAnimationManager.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace MedicalCard.Animations
+{
+    public class GridColumnAnimationManager
+    {
+        private List<GridColumnAnimation> _animations;
+        private ColumnDefinition _column;
+        private double _openWidth;
+
+        public int Duration { set; get; }
+        public int Fps { set; get; }
+        public AnimationState State { get; private set; }
+
+        public GridColumnAnimationManager(ColumnDefinition column, double openWidth, AnimationState state, int duration, int fps)
+        {
+            _column = column;
+            _openWidth = openWidth;
+            _animations = new List<GridColumnAnimation>();
+            State = state;
+            Duration = duration;
+            Fps = fps;
+
+            switch (State)
+            {
+                case AnimationState.Open:
+                    Dispatcher.CurrentDispatcher.Invoke(() => { _column.Width = new GridLength(openWidth, GridUnitType.Star); });
+                    break;
+                case AnimationState.Close:
+                    Dispatcher.CurrentDispatcher.Invoke(() => { _column.Width = new GridLength(0.0, GridUnitType.Star); });
+                    break;
+                default:
+                    throw new ArgumentException("Изначальное состояние меню может быть только Open или Close!");
+            }
+        }
+
+        public async void Open()
+        {
+            if (State == AnimationState.Open || State == AnimationState.Opening)
+            {
+                return;
+            }
+            if (State == AnimationState.Closing)
+            {
+                foreach (var anim in _animations)
+                {
+                    anim.Cancel();
+                }
+            }
+
+            var animation = new GridColumnAnimation(_column);
+            _animations.Add(animation);
+            State = AnimationState.Opening;
+
+            await animation.AnimateWidth(_openWidth, Convert.ToInt32(Duration - Duration * (_column.Width.Value / _openWidth)), Fps);
+
+            if (!animation.IsCancelled)
+            {
+                State = AnimationState.Open;
+            }
+            _animations.Remove(animation);
+        }
+
+        public async void Close()
+        {
+            if (State == AnimationState.Close || State == AnimationState.Closing)
+            {
+                return;
+            }
+            if (State == AnimationState.Opening)
+            {
+                foreach (var anim in _animations)
+                {
+                    anim.Cancel();
+                }
+            }
+
+            var animation = new GridColumnAnimation(_column);
+            _animations.Add(animation);
+            State = AnimationState.Closing;
+
+            await animation.AnimateWidth(0.0, Convert.ToInt32(Duration * (_column.Width.Value / _openWidth)), Fps);
+
+            if (!animation.IsCancelled)
+            {
+                State = AnimationState.Close;
+            }
+            _animations.Remove(animation);
+        }
+
+    }
+}
